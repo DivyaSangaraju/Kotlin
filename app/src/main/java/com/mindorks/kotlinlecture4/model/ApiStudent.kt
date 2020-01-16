3ffe43e68807024f747f1698de2e991603e04f5f
GuruCharan <gurucharanolati@gmail.com>
2020-01-16 11:54:12 +0530

    Initial commit

---
+++ app/src/main/java/com/mindorks/kotlinlecture4/model/ApiStudent.kt
@@ -0,0 +1,4 @@
+package com.mindorks.kotlinlecture4.model
+
+data class ApiStudent(val firsName: String, val lastName: String, val averageMark: Int)
+//data class ApiStudent1(val firsName1: String, val lastName1: String, val averageMark1: Int)
---
+++ app/src/main/java/com/mindorks/kotlinlecture4/model/Student.kt
@@ -0,0 +1,4 @@
+package com.mindorks.kotlinlecture4.model
+
+data class Student(val userName: String, val averageMark: Int)
+//data class Student1(val userName: String, val averageMark: Int)
